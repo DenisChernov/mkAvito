e6ba09c8917bd063572b2982d1f2ea3165ba70b4
Demiin <ch3rnov.denis@gmail.com>
2015-06-17 12:51:45 +0300

    created

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: demiin
+ *
+ * Created on June 17, 2015, 12:36 PM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
